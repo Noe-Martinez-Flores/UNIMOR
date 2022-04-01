74cdd738c146320490d6872adfdc1467049bcd5f
Noe-Martinez-Flores <20203tn035@utez.edu.mx>
2022-03-31 19:48:21 -0600

    message change

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView} from 'react-native'
 import React from 'react'
 import { Avatar, Input } from 'react-native-elements'
 import {MaterialIcons} from '@expo/vector-icons'
 
 
 export default function Comments() {
+
+
+    const datos = [
+        {
+            name : 'usuario1',
+            comentario : 'Es un buen Lugar'
+        },
+        {
+            name : 'usuario2',
+            comentario : 'Es un buen Lugar'
+        },
+        {
+            name : 'usuario3',
+            comentario : 'Es un buen Lugar'
+        }
+    ]
     return (
         <View style={styles.container}>
             <View>
@@ -20,13 +36,13 @@ export default function Comments() {
                     ></Avatar>
                 </View>
                 <View style = {{ width : 300}}>
-                    <Input  rightIcon= {<MaterialIcons name="send" size={20} color = {'gray'}/>} ></Input>
+                    <Input  rightIcon= {<MaterialIcons name="send" size={20} color = {'#299BF0'}/>} ></Input>
                 </View>
                 
             </View>
 
-            <View style={{ flexDirection: 'row', justifyContent : 'space-between' }} >
-                <View style = {{marginTop : 5}}>
+            <View style={{ flexDirection: 'column', justifyContent : 'space-between', backgroundColor : '#DDDDDD' , borderRadius : 15}} >
+                {/* <View style = {{marginTop : 5}}>
                     <Avatar
                         rounded
                         size={'medium'}
@@ -36,10 +52,30 @@ export default function Comments() {
                 <View style = {{ width : 300}}>
                     <Text style = {{marginStart : 10, fontWeight : 'bold', fontSize : 15}}>userName</Text>
                     <Input value='buen luegar' disabled color = {'black'} ></Input>
-                </View>
-                
-            </View>
+                </View> */}
 
+                {datos.map((dato, index) => {
+                        return (
+                           
+                            <View key={index} style = {{flexDirection : 'row'}}>
+                                <View style = {{ marginStart : 10, marginTop : 10 }}>
+                                    <Avatar
+                                    rounded
+                                    size={'medium'}
+                                    source={require('../../assets/userprofile.png')}
+                                ></Avatar>
+                                </View>
+                                <View style = {{ width : 300}}>
+                                <Text style = {{marginStart : 10, fontWeight : 'bold', fontSize : 15, marginTop : 5}}>{dato.name}</Text>
+                                <Input value={dato.comentario} disabled color = {'black'}></Input>
+                                </View>
+                            </View>
+                        )
+                    })}
+                <View style={{}}>
+                   
+                </View>
+            </View>
         </View>
     )
 }
